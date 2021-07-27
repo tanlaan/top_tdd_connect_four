7f2673751e14a245fe1a8681f5c04bd8f8659b10
Chris Bolas <tanlaan33@gmail.com>
2021-07-27 07:40:01 -0700

    Init board tests

---
+++ lib/connect_four.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+# Connect Four Game Implementation Class
+class ConnectFour
+  def initialize
+    board_height = 6
+    board_width  = 7
+    @board = Array.new(board_width) { Array.new(board_height) }
+  end
+
+  def play
+    # This in theory should just be a loop of various functions
+  end
+end
