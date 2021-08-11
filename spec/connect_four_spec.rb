eb07830515519d107795b6060859dd6482a5880c
Chris Bolas <tanlaan33@gmail.com>
2021-08-11 11:38:12 -0700

    Fix robocop complaint

--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -5,7 +5,7 @@ require_relative '../lib/connect_four'
 RSpec.describe ConnectFour do
   subject(:game_start) { described_class.new }
   context 'create a gameboard on startup' do
-    let(:start_board) { game_start.instance_variable_get(:@board)}
+    let(:start_board) { game_start.instance_variable_get(:@board) }
     it 'is not empty' do
       expect(start_board).to exist
     end
