81a0ab17b1449fe3b7f597998eb50b8b196480ac
Chris Bolas <tanlaan33@gmail.com>
2021-08-27 09:26:29 -0700

    Fix playable_game test

--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -3,11 +3,8 @@
 require_relative '../lib/connect_four'
 
 RSpec.describe ConnectFour do
-  subject(:game_start) { described_class.new }
-  context 'create a gameboard on startup' do
-    let(:start_board) { game_start.instance_variable_get(:@board) }
-    it 'is not empty' do
-      expect(start_board).to exist
-    end
+  let(:playable_game) { ConnectFour.new }
+  it 'can be played' do
+    expect(playable_game).to respond_to :play
   end
 end
