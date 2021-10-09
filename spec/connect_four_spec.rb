3a34a98fea870da1e4982448088f5d8fb5cc5503
Chris Bolas <tanlaan33@gmail.com>
2021-10-09 14:34:43 -0700

    Added tests (incorrect order for TDD)

--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -7,4 +7,18 @@ RSpec.describe ConnectFour do
   it 'can be played' do
     expect(playable_game).to respond_to :play
   end
+  let(:winnable_game) { ConnectFour.new }
+  it 'can be won' do
+    winnable_game_board = winnable_game.instance_variable_get(:@board)
+    4.times { winnable_game_board.move(0, 'x') }
+    expect(winnable_game.winner?).to be true
+  end
+  let(:not_winner) { ConnectFour.new }
+  it "knows when one of the tokens isn't the same" do
+    not_winner_board = not_winner.instance_variable_get(:@board)
+    2.times { not_winner_board.move(0, 'x') }
+    not_winner_board.move(0, 'o')
+    2.times { not_winner_board.move(0, 'x') }
+    expect(not_winner.winner?).to be false
+  end
 end
